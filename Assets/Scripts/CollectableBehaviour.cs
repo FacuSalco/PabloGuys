3f41b0af852090d38d5a514a026beab102f72d41
FacuSalco <101563649+FacuSalco@users.noreply.github.com>
2022-06-24 12:15:22 -0300

    Update_24/6/2022

--- Assets/Scripts/CollectableBehaviour.cs
+++ Assets/Scripts/CollectableBehaviour.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,18 +19,17 @@ public class CollectableBehaviour : MonoBehaviour
     {
         if (PauseScript.pausa == false) //Si no esta pausado el juego
         {
-            transform.Rotate(0, 1, 0);
 
             segundos = float.Parse(PlayerCollision._timer.text);
 
             if (segundos % 2 != 0)
             {
-                transform.Translate(0, 0.01f, 0);
+                transform.Translate(0 ,0.01f, 0);
             }
 
             else if (segundos % 2 == 0)
             {
-                transform.Translate(0,-0.01f, 0);
+                transform.Translate(0, -0.01f, 0);
             }
         }
 
